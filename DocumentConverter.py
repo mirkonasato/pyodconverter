fd8af59277b98cef7b4b191cce7db77c98852c67
Marcel Araujo <marcel.araujo@inphonex.com>
2013-02-07 16:05:29 -0200

    Updating Exception class because of a Python 2.6 deprecated class.
    Reference
    http://stackoverflow.com/questions/1272138/baseexception-message-deprecated-in-python-2-6

--- DocumentConverter.py
+++ DocumentConverter.py
@@ -261,11 +261,13 @@ IMAGES_MEDIA_TYPE = {
 
 class DocumentConversionException(Exception):
 
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return self.message
+    def _get_message(self): 
+        return self._message
+    
+    def _set_message(self, message): 
+        self._message = message
+    
+    message = property(_get_message, _set_message)
 
 
 class DocumentConverter:
